e12b6ee2dfd2d3a460da46875855d7381e166b77
Anto Subash <antosubash@live.com>
2021-12-01 00:17:14 +0100

    wip

---
+++ src/Migrations/20211130223859_Initial.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MinimalApiAbp.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Books",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    Name = table.Column<string>(type: "TEXT", maxLength: 128, nullable: false),
+                    ExtraProperties = table.Column<string>(type: "TEXT", nullable: true),
+                    ConcurrencyStamp = table.Column<string>(type: "TEXT", maxLength: 40, nullable: true),
+                    CreationTime = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    CreatorId = table.Column<Guid>(type: "TEXT", nullable: true),
+                    LastModificationTime = table.Column<DateTime>(type: "TEXT", nullable: true),
+                    LastModifierId = table.Column<Guid>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Books", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Books");
+        }
+    }
+}
--- src/Program.cs
+++ src/Program.cs
@@ -1,25 +1,124 @@
+using Microsoft.EntityFrameworkCore;
+using Volo.Abp;
+using Volo.Abp.AspNetCore.Mvc;
+using Volo.Abp.Autofac;
+using Volo.Abp.Domain.Entities.Auditing;
+using Volo.Abp.EntityFrameworkCore;
+using Volo.Abp.EntityFrameworkCore.Modeling;
+using Volo.Abp.EntityFrameworkCore.Sqlite;
+using Volo.Abp.Modularity;
+using Volo.Abp.Domain.Repositories.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
+builder.Host.AddAppSettingsSecretsJson()
+    .UseAutofac();
+builder.Services.ReplaceConfiguration(builder.Configuration);
+builder.Services.AddApplication<MinimalModule>();
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+// app.MapGet("/book", async (context) =>
+// {
+//     var repository = context.RequestServices.GetRequiredService<BookRepository>();
+//     await repository.GetListAsync();
+// });
+
+app.MapPost("/book", async (MyDbContext context, string name) =>
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    var newBook = context.Books.Add(new Book(name));
+    await context.SaveChangesAsync();
+    return Results.Created($"/book/{newBook.Entity.Id}", newBook.Entity);
+});
+
+app.InitializeApplication();
+app.Run();
+
+public class Book : AuditedAggregateRoot<Guid>
+{
+    public Book(string name)
+    {
+        this.Name = name;
+    }
+    public string Name { get; set; }
 }
 
-app.UseHttpsRedirection();
 
-app.UseAuthorization();
+public class MyDbContext : AbpDbContext<MyDbContext>
+{
+    public DbSet<Book> Books => Set<Book>();
 
-app.MapControllers();
+    public MyDbContext(DbContextOptions<MyDbContext> options)
+        : base(options)
+    {
 
-app.Run();
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        base.OnConfiguring(optionsBuilder);
+        optionsBuilder.UseSqlite();
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        //Always call the base method
+        base.OnModelCreating(builder);
+
+        builder.Entity<Book>(b =>
+        {
+            b.ToTable("Books");
+
+            //Configure the base properties
+            b.ConfigureByConvention();
+
+            //Configure other properties (if you are using the fluent API)
+            b.Property(x => x.Name).IsRequired().HasMaxLength(128);
+        });
+    }
+}
+
+public class BookRepository : EfCoreRepository<MyDbContext, Book, Guid>
+{
+    public BookRepository(IDbContextProvider<MyDbContext> dbContextProvider)
+        : base(dbContextProvider)
+    {
+    }
+}
+
+[DependsOn(
+    typeof(AbpAspNetCoreMvcModule),
+    typeof(AbpAutofacModule),
+    typeof(AbpEntityFrameworkCoreSqliteModule)
+)]
+public class MinimalModule : AbpModule
+{
+    public override void ConfigureServices(ServiceConfigurationContext context)
+    {
+        context.Services.AddEndpointsApiExplorer();
+        context.Services.AddSwaggerGen();
+        context.Services.AddAbpDbContext<MyDbContext>(options =>
+        {
+            options.AddDefaultRepositories(includeAllEntities: true);
+            options.AddRepository<Book, BookRepository>();
+        });
+
+
+        Configure<AbpDbContextOptions>(options =>
+        {
+            options.UseSqlite();
+        });
+    }
+
+    public override void OnApplicationInitialization(ApplicationInitializationContext context)
+    {
+        var app = context.GetApplicationBuilder();
+        var env = context.GetEnvironment();
+        // Configure the HTTP request pipeline.
+        if (env.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseHttpsRedirection();
+    }
+}
